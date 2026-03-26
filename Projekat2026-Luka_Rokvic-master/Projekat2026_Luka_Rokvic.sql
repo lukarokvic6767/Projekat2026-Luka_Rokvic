2bec0c69189a9af9decf5e424715107aed3f647e
lukarokvic6767 <luka.rokvic@prvabeogim.edu.rs>
2026-03-26 17:13:10 +0100

    Delete Projekat2026-Luka_Rokvic-master directory

--- Projekat2026-Luka_Rokvic-master/Projekat2026_Luka_Rokvic.sql
+++
@@ -1,9 +0,0 @@
-create database Projekat2026_Nikola_Milosevic
-
-USE Projekat2026_Nikola_Milosevic
-
-CREATE TABLE Korisnik(
-id INT IDENTITY(1,1) PRIMARY KEY,
-email VARCHAR(40),
-pass VARCHAR(30),
-uloga INT)
